e6dea74d1b16b5531652a4d81740ee64650cfa83
Vasilii Besschetnov <vbesschetnov@yandex.ru>
2018-01-12 22:59:01 -0800

    header's background is added. css reset file is added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "styles/_reset.scss";
 import Header from "components/Header/Header.jsx";
 
 const root = document.getElementById("Root");
