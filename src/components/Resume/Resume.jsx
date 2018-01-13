2ed877b2a2eb387ca40121d25ae2c63decde25e3
Vasilii Besschetnov <vbesschetnov@yandex.ru>
2018-01-13 11:08:30 -0800

    resets were added for hyperlinks. Resume.jsx uses css modules

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import "./_resume.scss";
+import { resume } from "./_resume.scss";
 import Header from "components/Header/Header.jsx";
 import MaxWidth from "components/MaxWidth/MaxWidth.jsx";
 
 const Resume = () => {
     return (
-        <div className="resume">
+        <div className={resume}>
             <Header />
             <MaxWidth>            
                 here would be a main content           
