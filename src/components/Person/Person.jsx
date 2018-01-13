0fcb86a7bc67974494c80f47650d98e6eff0a8f3
Vasilii Besschetnov <vbesschetnov@yandex.ru>
2018-01-12 19:23:18 -0800

    Person.jsx was refactored

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,19 +3,21 @@ import img from "images/MyPhoto_small.jpg";
 import "./_persons.scss";
 
 const Person = () => {
+    const fullName = "Vasilii Besschetnov",
+          position = "Lead Web Developer";
     return (
         <div className="cnt-person">
             <div className="cnt-photo">
                 <div className="person-photo">
-                    <img src={img} alt="Vasilii Besschetnov" />
+                    <img src={img} alt={fullName} />
                 </div>
             </div>
             <div className="cnt-name">
                 <div className="person-name">
-                    Vasilii Besschetnov
+                    {fullName}
                 </div>
                 <div className="person-position">
-                    Lead Web Developer
+                    {position}
                 </div>
             </div>
         </div>
